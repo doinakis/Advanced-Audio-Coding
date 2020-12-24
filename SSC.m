f0a26e7cf2e4dd7200d13062ca512e8dc2bc2f60
doinakis_laptop <doinakis.michalis@gmail.com>
2020-12-24 23:21:23 +0200

    Finish SSC implementation

--- SSC.m
+++ SSC.m
@@ -4,11 +4,11 @@
 %   ESH = EIGHT_SHORT_SEQUENCE
 %   LPS = LONG_STOP_SEQUENCE
 %%
-
-
 function frameType = SSC(frameT,nextframeT,prevframeType)
 
-H = @(z) (0.7548 - 0.7548 .* z^(-1))/(1 - 0.5095 .* z^(-1));
+
+a = [1 -0.5095];
+b = [0.7548 -0.7548];
 
 switch prevframeType
     case "LSS"
@@ -18,10 +18,69 @@ switch prevframeType
         frameType = "OLS";
         return;
     case "OLS"
+        filtered = filter(b,a,nextframeT,[],1);
+        s_values = NaN(8,2);
+        counter_bottom = 577;
+        counter_top = 685;
+        for i = 1:8
+            s_values(i,1) = sum(filtered(counter_bottom:counter_top,1).^2);
+            s_values(i,2) = sum(filtered(counter_bottom:counter_top,2).^2);
+            counter_bottom = counter_bottom + 128;
+            counter_top = counter_top + 128;
+        end
+        attack_values = s_values./((1/8)* sum(s_values,1));
         
+        if any(s_values(:,1) > 1e-3) && any(attack_values(:,1) > 10)
+            frame_channel_1 = "LSS";
+        else
+            frame_channel_1 = "OLS";
+        end
+        if any(s_values(:,2) > 1e-3) && any(attack_values(:,2) > 10)
+            frame_channel_2 = "LSS";
+        else
+            frame_channel_2 = "OLS";
+        end
+        if frame_channel_1 == frame_channel_2 
+            frameType = frame_channel_1;
+        elseif (frame_channel_1 == "OLS" && frame_channel_2 == "LSS") || (frame_channel_2 == "OLS" && frame_channel_1 == "LSS")
+            frameType = "LSS";
+        elseif (frame_channel_1 == "OLS" && frame_channel_2 == "LPS") || (frame_channel_2 == "OLS" && frame_channel_1 == "LPS")
+            frameType = "LPS";
+        else
+            frameType = "ESH";
+        end
     case "ESH"
+        filtered = filter(b,a,nextframeT,[],1);
+        s_values = NaN(8,2);
+        counter_bottom = 577;
+        counter_top = 685;
+        for i = 1:8
+            s_values(i,1) = sum(filtered(counter_bottom:counter_top,1).^2);
+            s_values(i,2) = sum(filtered(counter_bottom:counter_top,2).^2);
+            counter_bottom = counter_bottom + 128;
+            counter_top = counter_top + 128;
+        end
+        attack_values = s_values./((1/8)* sum(s_values,1));
+        
+        if any(s_values(:,1) > 1e-3) && any(attack_values(:,1) > 10)
+            frame_channel_1 = "ESH";
+        else
+            frame_channel_1 = "LPS";
+        end
+        if any(s_values(:,2) > 1e-3) && any(attack_values(:,2) > 10)
+            frame_channel_2 = "ESH";
+        else
+            frame_channel_2 = "LPS";
+        end
+        if frame_channel_1 == frame_channel_2 
+            frameType = frame_channel_1;
+        elseif (frame_channel_1 == "OLS" && frame_channel_2 == "LSS") || (frame_channel_2 == "OLS" && frame_channel_1 == "LSS")
+            frameType = "LSS";
+        elseif (frame_channel_1 == "OLS" && frame_channel_2 == "LPS") || (frame_channel_2 == "OLS" && frame_channel_1 == "LPS")
+            frameType = "LPS";
+        else
+            frameType = "ESH";
+        end
 end
-
-
 end
 
--- testing2.m
+++ testing2.m
@@ -23,31 +23,3 @@ for i = 1:2*ceil(size(y,1)/2048)+1
         break;
     end
 end
-
-
-b = [0.7548 -0.7548];
-a = [1 -0.5095];
-frameT = [frame1(:,1) frame2(:,1)];
-filtered = filter(b,a,frameT,[],1);
-
-s_values = NaN(8,2);
-counter_bottom = 577;
-counter_top = 685;
-for i = 1:8
-
-    s(i,1) = sum(filtered(counter_bottom:counter_top,1).^2);
-    s(i,2) = sum(filtered(counter_bottom:counter_top,2).^2);
-    counter_bottom = counter_bottom + 128;
-    counter_top = counter_top + 128;
-end
-
-attack_values = s./((1/8)* sum(s,1));
-
-counter_bottom = 449;
-counter_top = 704;
-for i = 1:8
-   subframe1(:,i) = frameT(counter_bottom:counter_top,1);
-   subframe2(:,i) = frameT(counter_bottom:counter_top,2);
-   counter_bottom = counter_bottom + 128;
-   counter_top = counter_top + 128;
-end
