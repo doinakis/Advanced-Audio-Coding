fc8dc08496298ce295ff1ee7e93d76a100caf632
doinakis_laptop <doinakis.michalis@gmail.com>
2020-12-24 03:46:12 +0200

    MODIFY: gitignore ADD:	Make the sound frames 	Progress to Sequence Segmentation Control

--- SSC.m
+++ SSC.m
@@ -1,4 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   OLS = ONLY_LONG_SEQUENCE
+%   LSS = LONG_START_SEQUENCE
+%   ESH = EIGHT_SHORT_SEQUENCE
+%   LPS = LONG_STOP_SEQUENCE
+%%
+
+
 function frameType = SSC(frameT,nextframeT,prevframeType)
+
+H = @(z) (0.7548 - 0.7548 .* z^(-1))/(1 - 0.5095 .* z^(-1));
+
+switch prevframeType
+    case "LSS"
+        frameType = "ESH";
+        return;
+    case "LPS"
+        frameType = "OLS";
+        return;
+    case "OLS"
+        
+    case "ESH"
+end
+
 
 end
 
